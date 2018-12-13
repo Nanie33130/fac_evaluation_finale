a76924d00c24aab2c0daf96c09b3c6e0379a8cbe
Mélanie Lebeau <32926288+Nanie33130@users.noreply.github.com>
2018-12-13 12:06:41 +0100

    Evaluation finale

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Energie Collective</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/mon_site.css" />
+</head>
+<body>
+    <div class="cacherSprite">
+        <?php
+        include 'logos/sprite_symbol_pictos.svg';
+        ?>
+    </div>
+    
+    <div class="menu">
+        <div class="logo">
+        </div>
+        <input class="burger" type="checkbox">
+        <nav>
+            <input type="search" placeholder="Rechercher...">
+        </nav>
+    </div>
+    <div class="header">
+        <div class="headerImg">
+            <img src="./Photos/20161202_101925.jpg" alt="Photo header" >
+        </div>
+        <div class="logoHead">
+            <img src="./img/favicon.ico" alt="Logo">
+        </div>
+        <div class="headerText">
+            <h1>
+                L'énergie est notre affaire
+            </h1>
+            <h2>
+                Investissons dans la transition énergétique
+            </h2>
+        </div>
+        <div class="zoneHead">
+            <div class="coop">
+                <img src="./img/collab.svg" alt="LogoCollab">
+                <h3>Coopérateurs</h3>
+                <h2>190</h2>
+            </div>
+            <div  class="install">
+                <img src="./img/panneau.svg" alt="LogoPanneau">
+                <h3>Installations</h3>
+                <h2>8</h2>
+            </div>
+            <div  class="production">
+                <img src="./img/eclair.svg" alt="LogoEclair">
+                <h3>Production Annuelle</h3>
+                <h2>141 MWH</h2>
+            </div>
+            <div class="capital">
+                <img src="./img/tirelire.svg" alt="LogoTirelire">
+                <h3>Capital</h3>
+                <h2>112950 €</h2>
+            </div>
+        </div>
+    </div>
+    
+    <div class="zone1">
+        <div class="zone1Left">
+            <h2>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod fringilla iaculis. Quisque luctus felis.
+            </h2>
+        </div>
+        <div class="zone1Right">
+            <p>
+                Mauris at dolor lacinia, porttitor purus id, finibus eros. In vitae blandit turpis. Proin eu tortor sit amet turpis sagittis aliquam porttitor vel tortor.
+            </p>
+            <ul>
+                <li>
+                    Praesent eu diam sit amet sapien faucibus congue.
+                </li>
+                <li>
+                    Nulla egestas odio eget massa tincidunt posuere.
+                </li>
+                <li>
+                    Donec euismod risus ac placerat pretium.
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="zone2">
+        <a href="#">Comprendre notre modèle de production</a>
+    </div>
+    <div class="zone3">
+                    <div class="zone3Left">
+                        <h2>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod fringilla iaculis. Quisque luctus felis.
+                        </h2>
+                    </div>
+                    <div class="zone3Right">
+                        <p>
+                            Mauris at dolor lacinia, porttitor purus id, finibus eros. In vitae blandit turpis. Proin eu tortor sit amet turpis sagittis aliquam porttitor vel tortor.
+                        </p>
+                        <ul>
+                            <li>
+                                Praesent eu diam sit amet sapien faucibus congue.
+                            </li>
+                            <li>
+                                Nulla egestas odio eget massa tincidunt posuere.
+                            </li>
+                            <li>
+                                Donec euismod risus ac placerat pretium.
+                            </li>
+                        </ul>
+                    </div>
+    </div>
+    <div class="zone4">
+        <a href="#">Souscrire</a>
+    </div>
+    <div class="zone5">
+        <div class="zone5Img">
+            <h2>ACTUALITES</h2>
+            <img src="./Photos/IMG_20170121_122143.jpg" alt="Photo Actualité">
+        </div>
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Vestibulum dignissim nisi in felis convallis pretium. Morbi blandit consequat ullamcorper. 
+                Suspendisse hendrerit, metus ut faucibus congue, enim orci efficitur nisl, sed varius dolor turpis sed quam.
+                 Suspendisse eget nisl rutrum dui rhoncus sollicitudin ut pulvinar nunc.
+                </p>
+        </div>
+    </div>
+    <div class="zone6">
+        <div class="zone6Img">
+            <h2>NOTRE DEMARCHE</h2>
+            <img src="./Photos/IMG_4106.jpg" alt="Photo Demarche">
+        </div>
+        <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Vestibulum dignissim nisi in felis convallis pretium. Morbi blandit consequat ullamcorper. 
+                    Suspendisse hendrerit, metus ut faucibus congue, enim orci efficitur nisl, sed varius dolor turpis sed quam.
+                     Suspendisse eget nisl rutrum dui rhoncus sollicitudin ut pulvinar nunc.
+                    </p>
+            </div>
+    </div>
+    <div class="zone7">
+            <div class="zone7Img">
+                    <h2>NOS INSTALLATIONS</h2>
+                    <img src="./Photos/IMG_4107.jpg" alt="Photo Installations">
+                </div>
+                <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Vestibulum dignissim nisi in felis convallis pretium. Morbi blandit consequat ullamcorper. 
+                            Suspendisse hendrerit, metus ut faucibus congue, enim orci efficitur nisl, sed varius dolor turpis sed quam.
+                             Suspendisse eget nisl rutrum dui rhoncus sollicitudin ut pulvinar nunc.
+                            </p>
+                    </div>
+    </div>
+</body>
+</html>
