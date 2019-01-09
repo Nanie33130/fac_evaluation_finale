48aca647566b3984aaa7299d7ae7a89698b882fc
Mélanie Lebeau <32926288+Nanie33130@users.noreply.github.com>
2019-01-09 17:31:45 +0100

    Nouvelle version eval

--- index.php
+++ index.php
@@ -8,149 +8,149 @@
     <link rel="stylesheet" type="text/css" media="screen" href="css/mon_site.css" />
 </head>
 <body>
+    <!-- INCLUDE SVG -->
     <div class="cacherSprite">
         <?php
-        include 'logos/sprite_symbol_pictos.svg';
+        include 'img/sprite_symbol_pictos.svg';
         ?>
-    </div>
-    
-    <div class="menu">
-        <div class="logo">
-        </div>
-        <input class="burger" type="checkbox">
-        <nav>
-            <input type="search" placeholder="Rechercher...">
-        </nav>
-    </div>
-    <div class="header">
-        <div class="headerImg">
-            <img src="./Photos/20161202_101925.jpg" alt="Photo header" >
-        </div>
-        <div class="logoHead">
-            <img src="./img/favicon.ico" alt="Logo">
-        </div>
-        <div class="headerText">
-            <h1>
-                L'énergie est notre affaire
-            </h1>
-            <h2>
-                Investissons dans la transition énergétique
-            </h2>
-        </div>
-        <div class="zoneHead">
-            <div class="coop">
-                <img src="./img/collab.svg" alt="LogoCollab">
-                <h3>Coopérateurs</h3>
-                <h2>190</h2>
+    </div>  
+    <div class="head">
+        <!-- Zone Header_Menu -->
+        <div class="menuHeader">
+            <div class="logoHead">
+                <img src="logos/EnergiesCollectives_rectangle_Logo_ajuste.svg" alt="Logo EnergiesCollectives">
             </div>
-            <div  class="install">
-                <img src="./img/panneau.svg" alt="LogoPanneau">
-                <h3>Installations</h3>
-                <h2>8</h2>
+            
+            <div class="menu">
+                <svg class="Icon burger">
+                    <use xlink:href="#menu_burger"></use>
+                </svg>
             </div>
-            <div  class="production">
-                <img src="./img/eclair.svg" alt="LogoEclair">
-                <h3>Production Annuelle</h3>
-                <h2>141 MWH</h2>
+            
+        </div>
+        <!-- Zone Header_Img -->
+        <div class="photoHeader">
+            <img src="Photos/20161202_101925.jpg" alt="Photo_Header">                
+        </div>
+    </div>
+    <!-- Zone qui contient les éléments apres le header -->
+    <div class="container">
+        <!-- Zone Header -->
+        
+        
+        <div class="zoneIcone">
+            <div class="logoZoneIcone">
+                <img src="logos/EnergiesCollectivesLogo_ss_texte.svg" alt="Logo EnergieCollective sans texte">
             </div>
-            <div class="capital">
-                <img src="./img/tirelire.svg" alt="LogoTirelire">
-                <h3>Capital</h3>
-                <h2>112950 €</h2>
+            
+            <div class="texteZoneIcone">
+                <h1>
+                    L'Energie est notre affaire
+                </h1>
+                <h3>Investissons dans la transition énergétique</h3>
             </div>
-        </div>
-    </div>
-    
-    <div class="zone1">
-        <div class="zone1Left">
-            <h2>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod fringilla iaculis. Quisque luctus felis.
-            </h2>
-        </div>
-        <div class="zone1Right">
-            <p>
-                Mauris at dolor lacinia, porttitor purus id, finibus eros. In vitae blandit turpis. Proin eu tortor sit amet turpis sagittis aliquam porttitor vel tortor.
-            </p>
-            <ul>
-                <li>
-                    Praesent eu diam sit amet sapien faucibus congue.
-                </li>
-                <li>
-                    Nulla egestas odio eget massa tincidunt posuere.
-                </li>
-                <li>
-                    Donec euismod risus ac placerat pretium.
-                </li>
-            </ul>
-        </div>
-    </div>
-    <div class="zone2">
-        <a href="#">Comprendre notre modèle de production</a>
-    </div>
-    <div class="zone3">
-                    <div class="zone3Left">
-                        <h2>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod fringilla iaculis. Quisque luctus felis.
-                        </h2>
-                    </div>
-                    <div class="zone3Right">
-                        <p>
-                            Mauris at dolor lacinia, porttitor purus id, finibus eros. In vitae blandit turpis. Proin eu tortor sit amet turpis sagittis aliquam porttitor vel tortor.
-                        </p>
-                        <ul>
-                            <li>
-                                Praesent eu diam sit amet sapien faucibus congue.
-                            </li>
-                            <li>
-                                Nulla egestas odio eget massa tincidunt posuere.
-                            </li>
-                            <li>
-                                Donec euismod risus ac placerat pretium.
-                            </li>
-                        </ul>
-                    </div>
-    </div>
-    <div class="zone4">
-        <a href="#">Souscrire</a>
-    </div>
-    <div class="zone5">
-        <div class="zone5Img">
-            <h2>ACTUALITES</h2>
-            <img src="./Photos/IMG_20170121_122143.jpg" alt="Photo Actualité">
-        </div>
-        <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Vestibulum dignissim nisi in felis convallis pretium. Morbi blandit consequat ullamcorper. 
-                Suspendisse hendrerit, metus ut faucibus congue, enim orci efficitur nisl, sed varius dolor turpis sed quam.
-                 Suspendisse eget nisl rutrum dui rhoncus sollicitudin ut pulvinar nunc.
-                </p>
-        </div>
-    </div>
-    <div class="zone6">
-        <div class="zone6Img">
-            <h2>NOTRE DEMARCHE</h2>
-            <img src="./Photos/IMG_4106.jpg" alt="Photo Demarche">
-        </div>
-        <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Vestibulum dignissim nisi in felis convallis pretium. Morbi blandit consequat ullamcorper. 
-                    Suspendisse hendrerit, metus ut faucibus congue, enim orci efficitur nisl, sed varius dolor turpis sed quam.
-                     Suspendisse eget nisl rutrum dui rhoncus sollicitudin ut pulvinar nunc.
-                    </p>
-            </div>
-    </div>
-    <div class="zone7">
-            <div class="zone7Img">
-                    <h2>NOS INSTALLATIONS</h2>
-                    <img src="./Photos/IMG_4107.jpg" alt="Photo Installations">
+            
+            <div class="pictoZoneIcone">
+                <div class="pictoCoop">
+                    <svg class="coop" aria-hidden="true">
+                        <use xlink:href="#picto_coop" ></use>
+                    </svg>
+                    <h3>coopérateurs</h3>
+                    <h3>190</h3>
                 </div>
-                <div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Vestibulum dignissim nisi in felis convallis pretium. Morbi blandit consequat ullamcorper. 
-                            Suspendisse hendrerit, metus ut faucibus congue, enim orci efficitur nisl, sed varius dolor turpis sed quam.
-                             Suspendisse eget nisl rutrum dui rhoncus sollicitudin ut pulvinar nunc.
-                            </p>
-                    </div>
+                <div class="pictoPanneau">
+                    <svg class="panneau" aria-hidden="true">
+                        <use xlink:href="#picto_photovoltaique" ></use>
+                    </svg>
+                    <h3>installations</h3>
+                    <h3>8</h3>
+                </div>
+                <div class="pictoEnergie">
+                    <svg class="energie" aria-hidden="true">
+                        <use xlink:href="#picto_production_energie" ></use>
+                    </svg>
+                    <h3>production</h3> <h3>annuelle</h3>
+                    <h3>141 MWh</h3>
+                </div>
+                <div class="pictoInvestisseur">
+                    <svg class="invest" aria-hidden="true">
+                        <use xlink:href="#picto_capital" ></use>
+                    </svg>
+                    <h3>capital</h3>
+                    <h3>112950€</h3>
+                </div>
+            </div>
+            
+        </div>
+        <div class="zoneText">
+            <div class="zone1">
+                <div class="zone1Left">
+                    <h2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod fringilla iaculis. Quisque luctus felis.
+                    </h2>
+                </div>
+                <div class="zone1Right">
+                    <p>
+                        Mauris at dolor lacinia, porttitor purus id, finibus eros. In vitae blandit turpis. Proin eu tortor sit amet turpis sagittis aliquam porttitor vel tortor.
+                    </p>
+                    <ul>
+                        <li>
+                            Praesent eu diam sit amet sapien faucibus congue.
+                        </li>
+                        <li>
+                            Nulla egestas odio eget massa tincidunt posuere.
+                        </li>
+                        <li>
+                            Donec euismod risus ac placerat pretium.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="zone2">
+                <a href="#">Comprendre notre modèle de production</a>
+            </div>
+            
+            <div class="zone3">
+                <div class="zone3Left">
+                    <h2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod fringilla iaculis. Quisque luctus felis.
+                    </h2>
+                </div>
+                <div class="zone3Right">
+                    <p>
+                        Mauris at dolor lacinia, porttitor purus id, finibus eros. In vitae blandit turpis. Proin eu tortor sit amet turpis sagittis aliquam porttitor vel tortor.
+                    </p>
+                    <ul>
+                        <li>
+                            Praesent eu diam sit amet sapien faucibus congue.
+                        </li>
+                        <li>
+                            Nulla egestas odio eget massa tincidunt posuere.
+                        </li>
+                        <li>
+                            Donec euismod risus ac placerat pretium.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="zone4">
+                <a href="#">Souscrire</a>
+                <svg class="souscrireSVG" aria-hidden="true">
+                    <use xlink:href="#schema_souscrire"></use>
+                </svg>
+            </div>
+            
+        </div>
     </div>
-</body>
-</html>
+    
+    <!-- <svg class="Icon" aria-hidden="true">
+        <use xlink:href="#fleche_droite"></use
+        </svg> -->
+        
+        <footer class="footer">
+            
+        </footer>
+    </body>
+    </html>
